0d31c52219adac9ec7f1cabbcb69a9c04b16ef41
shakycode <jelinek@gmail.com>
2016-03-28 07:36:50 -0500

    Setup show action

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,8 +4,12 @@ class EmailsController < ApplicationController
     @emails = current_user.emails.order("created_at DESC")
   end
 
-  def burn_it
-    current_user.burn_email
-    redirect_to emails_path, notice: "You now have a new email address, enjoy it for the day!"
+  def show
+    @email = Email.find(params[:id])
   end
+
+  # def burn_it
+  #   current_user.burn_email
+  #   redirect_to emails_path, notice: "You now have a new email address, enjoy it for the day!"
+  # end
 end

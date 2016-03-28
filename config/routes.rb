6e8fa696f575d36bd895bb756057c9c1c24a7983
shakycode <jelinek@gmail.com>
2016-03-28 07:37:05 -0500

    Resources/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   mount_griddler
+  resources :emails
   root 'emails#index'
   #post '/email_processor' => 'emails#process'
   # The priority is based upon order of creation: first created -> highest priority.

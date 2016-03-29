c1f149dbb35d1b8c1c62dd837d885652f5cd13d0
shakycode <jelinek@gmail.com>
2016-03-28 20:29:34 -0500

    Add delete/destroy method.. we may want to paginate

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -8,8 +8,14 @@ class EmailsController < ApplicationController
     @email = Email.find(params[:id])
   end
 
-  # def burn_it
-  #   current_user.burn_email
-  #   redirect_to emails_path, notice: "You now have a new email address, enjoy it for the day!"
-  # end
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    redirect_to emails_path, notice: "Message Deleted!"
+  end
+
+  def burn_it
+     current_user.burn_email
+     redirect_to emails_path, notice: "You now have a new email address, enjoy it for the day!"
+  end
 end

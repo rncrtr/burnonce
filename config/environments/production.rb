b8a6f734d82a81ef1245a888d45edbe4f4095d67
shakycode <shakycode@shakycodes-MacBook-Pro.local>
2016-04-02 07:16:55 -0500

    Setup dev/prod email env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #setup production mailing environment
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "localhost",
+    :port                 => 25,
+    :openssl_verify_mode => 'none'
+  }
+  config.action_mailer.default_url_options = { :host => 'https://burnonce.com' }
 end

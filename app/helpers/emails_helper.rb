2216bfb23186aaef0f01b0886522ff86d11ab4d3
shakycode <jelinek@gmail.com>
2016-04-06 15:15:37 -0500

    Filters

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -5,10 +5,10 @@ module EmailsHelper
     pipeline = HTML::Pipeline.new [
       HTML::Pipeline::MarkdownFilter,
       #HTML::Pipeline::AbsoluteSourceFilter,
-      HTML::Pipeline::AutolinkFilter,
+      #HTML::Pipeline::AutolinkFilter,
       #HTML::Pipeline::ImageMaxWidthFilter,
       #HTML::Pipeline::EmailReplyFilter,
-      HTML::Pipeline::SanitizationFilter
+      HTML::Pipeline::SanitizationFilter,
     ], pipeline_context
     pipeline.call(body)[:output].to_s.html_safe
   end

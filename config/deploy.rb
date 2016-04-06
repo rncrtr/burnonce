e5391980444f94744b6b532822c132173fd3a724
shakycode <jelinek@gmail.com>
2016-04-05 21:04:08 -0500

    Remove bin linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repo_url, 'git@github.com:shakycode/burnonce.git'
 set :deploy_to, '/home/deploy/burnonce'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 

f4218d72c3db75b06e081c0d53d79a1b1ec90c6a
shakycode <jelinek@gmail.com>
2016-04-06 14:10:41 -0500

    stupid filters

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -4,12 +4,12 @@ module EmailsHelper
     pipeline_context = { gfm: false }
     pipeline = HTML::Pipeline.new [
       HTML::Pipeline::MarkdownFilter,
-      HTML::Pipeline::AbsoluteSourceFilter,
+      #HTML::Pipeline::AbsoluteSourceFilter,
       HTML::Pipeline::AutolinkFilter,
-      HTML::Pipeline::ImageMaxWidthFilter,
-      HTML::Pipeline::EmailReplyFilter,
+      #HTML::Pipeline::ImageMaxWidthFilter,
+      #HTML::Pipeline::EmailReplyFilter,
       HTML::Pipeline::SanitizationFilter
     ], pipeline_context
-    pipeline.call(body)[:output].to_s
+    pipeline.call(body)[:output].to_s.html_safe
   end
 end

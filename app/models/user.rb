1edf6e9e8c26c36fbf2055ae8e69dafdf3256703
shakycode <jelinek@gmail.com>
2016-04-19 08:35:41 -0500

    add another domain

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
 
   def generate_burn_email
     chars = Array('a'..'z')
-    domain = ["@burnonce.xyz", "@burnonce.com", "@burnonce.tech"]
+    domain = ["@burnonce.xyz", "@burnonce.com", "@burnonce.tech", "@burnonce.pw"]
     self.burn_email = Array.new(10) { chars.sample }.join + "#{domain.sample}"
     self.last_burn = Time.zone.now
     self.save

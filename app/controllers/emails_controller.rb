78aee18fd4958b69f93f67722ab7fd2ccd6240db
shakycode <jelinek@gmail.com>
2016-05-09 07:55:42 -0500

    Add new and reply emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -8,6 +8,34 @@ class EmailsController < ApplicationController
     @email = current_user.emails.find(params[:id])
   end
 
+  def new
+    @email = current_user.emails.new
+  end
+  def create
+        @email = current_user.emails.new(email_params)
+        respond_to do |format|
+          if @email.save
+            EmailMailer.new_or_reply(@email).deliver_now
+            format.html { redirect_to emails_path, notice: "Awesome #{current_user.username} your email has been sent!" }
+            format.json { render action: 'show', status: :created, location: @email }
+          else
+            format.html { render action: 'reply' }
+            format.json { render json: @email.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+  def reply
+    original_email = current_user.emails.find(params[:id])
+    @email = Email.new(
+      user_id: original_email.user_id,
+      from_email: original_email.to_email,
+      to_email: original_email.from_email,
+      subject: original_email.body,
+      body: original_email.body
+    )
+  end
+
   def destroy
     @email = Email.find(params[:id])
     @email.destroy
@@ -23,5 +51,10 @@ class EmailsController < ApplicationController
   def burn_it
      current_user.generate_burn_email
      redirect_to emails_path, notice: "You now have a new email address, enjoy it while it lasts!"
+  end
+
+private
+  def email_params
+    params.require(:email).permit(:from_email, :to_email, :subject, :body, :user_id)
   end
 end
